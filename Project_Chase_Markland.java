c01c46163e9ceba5e0a84a8408717d44c8deb637
Chasem-2 <36250516+Chasem-2@users.noreply.github.com>
2022-10-10 00:40:15 -0400

    Step 2.2
    Modified to read input from a file.

--- Project_Chase_Markland.java
+++ Project_Chase_Markland.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class Project_Chase_Markland {
     public static void main(String[] args){
@@ -7,41 +9,54 @@ public class Project_Chase_Markland {
         int policyNumber, age;
         double height, weight;
         String providerName, phFname, phLname, issmoker;
+        //create a scanner instance to import the file
+        Scanner file;
+        //create array list to store policy objects loaded from file
+        ArrayList<Policy> policyarray=new ArrayList<Policy>();
 
-        //create an instance of scanner
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Please enter the Policy Number: ");
-        policyNumber = Integer.parseInt(sc.nextLine());
-        System.out.print("Please enter the Provider Name: ");
-        providerName = sc.nextLine();
-        System.out.print("Please enter the Policyholder's First Name: ");
-        phFname = sc.nextLine();
-        System.out.print("Please enter the Policyholder's Last Name: ");
-        phLname = sc.nextLine();
-        System.out.print("Please enter the Policyholder's Age: ");
-        age = Integer.parseInt(sc.nextLine());
-        System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
-        String smoker = sc.nextLine();
-        if(smoker.equals("smoker"))
-        issmoker = "Smoker";
-        else
-        issmoker = "Non-Smoker";
-        System.out.print("Please enter the Policyholder's Height (in inches): ");
-        height = sc.nextDouble();
-        System.out.print("Please enter the Policyholder's Weight (in pounds): ");
-        weight = sc.nextDouble();
-        //Creates a new instance of the policy class with the given user input
-        Policy policy = new Policy(policyNumber, providerName, phFname, phLname, age, issmoker, height, weight);
-        //Outputs information from the policy class
-        System.out.printf("Poilcy Number: %d\n",policy.getPNumber());
-        System.out.printf("Provider Name: %s\n",policy.getName());
-        System.out.printf("Policyholder's First Name: %s\n", policy.getphFname());
-        System.out.printf("Policyholder's Last Name: %s\n",policy.getphLname());
-        System.out.printf("Policyholder's Age: %d\n",policy.getAge());
-        System.out.printf("Policyholder's Smoking Status: $s\n",policy.isSmoker());
-        System.out.printf("Policyholder's Height: %.2f\n",policy.getHeight());
-        System.out.printf("Policyholder's Weight: %.2f\n",policy.getWeight());
-        System.out.printf("Policyholder's BMI: %.2f\n",policy.getBMI());
-        System.out.printf("Policy Price: $%.2f\n",policy.PolicyTotal());
+        //import information policy
+        try {
+            file = new Scanner(new File("PolicyInformation.txt"));
+            while(file.hasNext()){
+                policyNumber = file.nextInt();
+                providerName = file.nextLine();
+                phFname = file.nextLine();
+                phLname = file.nextLine();
+                age = file.nextInt();
+                issmoker = file.nextLine();
+                height = file.nextDouble();
+                weight = file.nextDouble();
+                //catch empty line between value sets
+                if(file.hasNext()){
+                    file.nextLine();
+                }
+                //creates policy object
+                Policy policy = new Policy(policyNumber, providerName, phFname, phLname, age, issmoker, height, weight);
+                //adds object to policyarray
+                policyarray.add(policy);
+
+
+                //Outputs information from the policy class
+                System.out.printf("Poilcy Number: %d\n",policy.getPNumber());
+                System.out.printf("Provider Name: %s\n",policy.getName());
+                System.out.printf("Policyholder's First Name: %s\n", policy.getphFname());
+                System.out.printf("Policyholder's Last Name: %s\n",policy.getphLname());
+                System.out.printf("Policyholder's Smoking Status: $s\n",policy.isSmoker());
+                System.out.printf("Policyholder's Age: %d\n",policy.getAge());
+                System.out.printf("Policyholder's Weight: %.2f\n",policy.getWeight());
+                System.out.printf("Policyholder's Height: %.2f\n",policy.getHeight());
+                System.out.printf("Policy Price: $%.2f\n",policy.PolicyTotal());
+                System.out.printf("Policyholder's BMI: %.2f\n",policy.getBMI());
+
+
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+
+        
     }    
 }
